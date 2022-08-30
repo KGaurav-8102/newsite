d75fcd6186ee016f8cdd37fbdf3ee47967ccab5a
Gaurav Yadav <gk5480325@gmail.com>
2022-08-30 17:48:50 +0530

    Books models done

--- mysite/books/models.py
+++ mysite/books/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
+
+    def __str__(self):
+        return self.name + '-' + self.author
+
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     price = models.CharField(max_length=100)
