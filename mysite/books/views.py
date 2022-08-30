11d6518fcd4ccba15e7fc64174e7810f210799d8
Gaurav Yadav <gk5480325@gmail.com>
2022-08-30 19:11:42 +0530

    extracting data from the database

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -3,6 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Book
 
 def index(request):
-    return HttpResponse("<h>This is the Books Homepage!</h>")
+    all_books = Book.objects.all()
+    html = ''
+    for book in all_books:
+        path = '/books/' + str(book.id) + '/'
+        html += '<a href = "' + path + '">' + str(book.name) + '</a><br>'
+
+    return HttpResponse(html)
+
+def detail(request, book_id):
+    return HttpResponse("<h2>Details for Book ID:" + str(book_id) + "</h2>")
