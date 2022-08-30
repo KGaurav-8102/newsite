d913acab0944afccd75572707cceb77313959d44
Gaurav Yadav <gk5480325@gmail.com>
2022-08-30 19:36:42 +0530

    render from django shortcuts used

--- mysite/books/views.py
+++ mysite/books/views.py
@@ -4,15 +4,14 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import Book
-from django.template import loader
+from django.shortcuts import render
 
 def index(request):
     all_books = Book.objects.all()
-    template = loader.get_template('books/index.html')
     context = {
         'all_books' : all_books
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'books/index.html', context)
 
 def detail(request, book_id):
     return HttpResponse("<h2>Details for Book ID:" + str(book_id) + "</h2>")
